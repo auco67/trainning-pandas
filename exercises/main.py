d0cc1b89cab976cc27f2f771bcfed8cf7c5c282b
auco67 <auco67@gmail.com>
2026-05-26 12:12:16 +0900

    add: 19～20

--- exercises/main.py
+++ exercises/main.py
@@ -2,20 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-    csv_file = "./datas/weather.csv"
-    df = pd.read_csv(csv_file, sep=",")
-    df = df[['年月日', '平均気温(℃)', '最高気温(℃)','最低気温(℃)']][1:51]
-    print(df)
-    plt.plot(df["年月日"], df['平均気温(℃)'], label="average")
-    plt.plot(df["年月日"], df['最高気温(℃)'], label="maximum")
-    plt.plot(df["年月日"], df['最低気温(℃)'], label="lowest")
-    plt.title("50 days")
-    plt.xlabel("date")
-    plt.ylabel("temp(℃)")
-    plt.xticks(range(0,len(df),5),df["年月日"][::5])
-    plt.tight_layout()
-    plt.legend()
-    plt.show()
+    csv_file = './datas/weather.csv'
+    csv_output = './datas/export.csv'
+    df = pd.read_csv(csv_file, sep=',')
+    df = df[['年月日', '平均気温(℃)', '最高気温(℃)', '最低気温(℃)', '降水量の合計(mm)',  '最深積雪(cm)', '平均雲量(10分比)',  '平均蒸気圧(hPa)', '平均風速(m/s)','日照時間(時間)']]
+    df = df.fillna(0)
+    print(df[1:])
+    df.to_csv(csv_output, index=False)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
